13f2faf35d98996985c8d8b4c0860cbb8e22d643
Md. Rakib <mdrakib5561@gmail.com>
2022-04-24 00:57:58 +0600

    variable

---
+++ Week 1/Module 1/Program 2/variable.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+
+    int phone;
+    char Name;
+
+    printf("Enter Your Name :");
+    scanf("%c", &Name);
+
+    printf("Enter you Phone : ");
+    scanf("%d", &phone);
+
+     printf("YOur Name is : %c\n", Name);
+    printf("Your Number is : %d\n", phone);
+    return 0;
+}
