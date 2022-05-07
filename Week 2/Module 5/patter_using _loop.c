d64b31ba721df4f45e86e6a7021310445722e2f3
Md. Rakib <mdrakib5561@gmail.com>
2022-05-07 23:37:28 +0600

    pattern useing loop

---
+++ Week 2/Module 5/patter_using _loop.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+
+
+    int n;
+    scanf("%d", &n);
+
+    printf("**************\n");
+
+    for(int i=0; i<=n-2; i++){
+        printf("*            *\n");
+    }
+
+    printf("**************\n");
+
+}
