8fcc85a027f86eeefbead48f0fda739c8ccd27a1
Md. Rakib <mdrakib5561@gmail.com>
2022-05-07 19:10:24 +0600

    module 4 start

---
+++ Week 2/Module 4/if_else program.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+
+    int marks;
+
+    scanf("%d", &marks);
+    if(marks >= 40){
+        printf("You have passed \n");
+    }
+    else{
+        printf("You have failed");
+    }
+
+
+}
