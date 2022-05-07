c465785a3a0c292a6ba15b490a6abcbb3c232394
Md. Rakib <mdrakib5561@gmail.com>
2022-05-07 23:24:19 +0600

    Namota using for loop

---
+++ Week 2/Module 5/loop_program.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+
+    int a , i;
+    printf("Enter a Number : ");
+    scanf("%d",&a);
+
+    for(i = 1; i<=10; i++){
+
+        printf("%d x %d = %d \n", a, i, a*i);
+
+    }
+
+    return 0;
+}
